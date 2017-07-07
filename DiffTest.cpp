92933ee35c3538244271aeef28298a87bd314df3
gang zhang <gang.zhang@pps.io>
2017-07-06 21:58:55 -0400

    adding more

--- DiffTest.cpp
+++ DiffTest.cpp
@@ -44,7 +44,9 @@ std::vector<Mat> breakImagesHor(Mat original) {
 				}
 			}
 		}
-		else {
+		
+		
+		if (xstart >= 0){
 			bool ended = true;
 			for (int j = 0; j < original.rows; ++j)
 			{
@@ -55,11 +57,16 @@ std::vector<Mat> breakImagesHor(Mat original) {
 				}
 			}
 			if (ended) {
-				int besth = bottomy - topy;
-				Mat digit(besth, x - xstart, original.type());
-				original(Rect(xstart, topy, x - xstart, besth)).copyTo(digit);
-				imgs.push_back(digit);
+				int besth = bottomy - topy + 1;
+				int iw = x - xstart;
+				if (besth > 1 && iw > 1) {
+					Mat digit(besth+2, iw+2, original.type(), Scalar(0));				
+					original(Rect(xstart, topy, iw, besth)).copyTo(digit(Rect(1,1,iw,besth)));
+					imgs.push_back(digit);
+				}
 				xstart = -1;
+				bottomy = original.rows;
+				topy = -1;
 			}
 		}
 	}
@@ -67,46 +74,200 @@ std::vector<Mat> breakImagesHor(Mat original) {
 	return imgs;
 }
 
-void doTopNumbers() {
-	int yStart = 90;
-	int yEnd = 150;
-	int xStart = 1450;
-	
-	Mat img = getGrayScale(imread("data\\cctxt\\lib1.jpg", IMREAD_COLOR));
+struct BlockInfo {
+public :	
+	Rect rect;
+	char * info;
+	int Threadshold;
+	BlockInfo(Rect r, int threadshold = 230, char* inf= NULL) {
+		rect = r;
+		info = inf;
+		Threadshold = threadshold;
+	}
+};
 
+Mat loadImageRect(Mat img, BlockInfo blk) {
+	Rect rect = blk.rect;
+	if (rect.width <= 0) {
+		rect.width = img.cols - rect.x;
+	}
 	Mat numBlock;
-	numBlock.create( yEnd - yStart, img.size().width - xStart, img.type());
-
+	numBlock.create(rect.height, rect.width, img.type());
 	for (int j = 0; j < numBlock.rows; ++j)
 	{
-		const uchar* current = img.ptr<uchar>(j + yStart);
+		const uchar* current = img.ptr<uchar>(j + rect.y);
 
 		uchar* output = numBlock.ptr<uchar>(j);
 
 		for (int i = 0; i < (numBlock.cols); i++)
 		{
-			*output = current[i + xStart];
+			uchar cval = current[i + rect.x];
+			*output = cval;
 			*output = 0;
-			if (current[i + xStart] > 230) {
+			if (cval > blk.Threadshold) {
 				*output = 255;
 			}
 			output++;
 		}
 	}
 
+	return numBlock;
+}
+void doTopNumbers(BlockInfo blk) {
+	
+	Mat img = getGrayScale(imread("data\\cctxt\\archerl1.JPG", IMREAD_COLOR));
+	Mat numBlock =loadImageRect(img, blk);
+	
 
-	namedWindow("test", WINDOW_NORMAL);	
-	imshow("test", numBlock);
+	char buf[512];
+	sprintf_s(buf, "tstimgs\\test%s_%i.png", blk.info, -1);
+	imwrite(buf, numBlock);
 	std::vector<Mat> digits = breakImagesHor(numBlock);
-	for (int i = 0; i < digits.size(); i++) {
-		char buf[512];
-		sprintf_s(buf, "test_%i.jpg", i);
+	for (int i = 0; i < digits.size(); i++) {		
+		sprintf_s(buf, "tstimgs\\test%s_%i.png", blk.info, i);
 		imwrite(buf, digits[i]);
+	}
+}
+
+struct RecoInfo{
+public:
+	Mat img;
+	char chr;
+	RecoInfo(Mat i, char ch) {
+		img = i;
+		chr = ch;
+	}
+};
+
+struct RecoList {
+public:
+	vector<RecoInfo> recoInfo;
+};
+
+int StrEndsWith(char *str, char * ending)
+{
+	if (strlen(str) >= strlen(ending))
+	{
+		if (!strcmp(str + strlen(str) - strlen(ending), ending))
+		{
+			return 1;
+		}
+	}
+	return 0;
+}
+
+RecoList LoadDataInfo(const char * dir) {
+	DIR *d = opendir(dir);
+	dirent * de;
+	RecoList res;
+	while (de = readdir(d)) {
+		if (StrEndsWith(de->d_name, ".png")) {
+			printf("%s %c\n", de->d_name, de->d_name[2]);
+			char buf[512];
+			sprintf_s(buf, "%s\\%s", dir, de->d_name);
+			Mat img = getGrayScale(imread(buf));
+			res.recoInfo.push_back(RecoInfo(img, de->d_name[2]));
+		}
+	}
+	closedir(d);
+	return res;
+}
+
+
+struct ImageDiffVal {
+public:
+	int x;
+	int y;
+	float val;
+	ImageDiffVal(int xx, int yy, float v) {
+		x = xx;
+		y = yy;
+		val = v;
+	}
+	bool operator<(const ImageDiffVal &rhs) const { return val < rhs.val; }
+};
+
+vector<ImageDiffVal> GetTopXGoodones(Mat result, int max) {
+	vector<ImageDiffVal> vals;
+	for (int y = 0; y < result.rows; y++) {
+		float* current = result.ptr<float>(y);
+		for (int x = 0; x < result.cols; x++) {
+			float pt = current[x];
+			vals.push_back(ImageDiffVal(x, y, pt));
+			sort(vals.begin(), vals.end());
+			if (vals.size() > max) {
+				vals.pop_back();
+			}
+		}
+	}
+	return vals;
+}
+void DoReco(RecoList list, Mat img) {
+	for (int i = 0; i < list.recoInfo.size(); i++) {
+		int match_method = CV_TM_SQDIFF;
+		Mat result;
+		RecoInfo recInfo = list.recoInfo[i];
+		Mat templ = recInfo.img;
+		printf("checking %c\n", recInfo.chr);
+		int result_cols = img.cols - templ.cols + 1;
+		int result_rows = img.rows - templ.rows + 1;
+		result.create(result_rows, result_cols, CV_32FC1);
+		matchTemplate(img, templ, result, match_method);
+
+
+		double minVal; double maxVal; Point minLoc; Point maxLoc;
+		Point matchLoc;
+		minMaxLoc(result, &minVal, &maxVal, &minLoc, &maxLoc, Mat());		
+		normalize(result, result, 1,0, NORM_MINMAX, -1, Mat());
+
+		
+		minMaxLoc(result, &minVal, &maxVal, &minLoc, &maxLoc, Mat());
+		char buf[512];
+		sprintf_s(buf, "tstimgs\\a_check%c_%i.png", recInfo.chr, i);
+		imwrite(buf, result);
+
+		int steps[10];		
+		for (int y = 0; y < 10; y++) steps[y] = 0;
+		for (int y = 0; y < result.rows; y++) {
+			float* current = result.ptr<float>(y);
+			for (int x = 0; x < result.cols; x++) {
+				int pt = (current[x] * 10);
+				if (pt >= 10) pt = 9;
+				if (pt < 0) {
+					pt = 0; 
+				}
+				if (pt >1) current[x] = 255;
+				else printf("found low at %i %i %f\n", x, y, current[x]);
+				steps[pt]++;
+			}
+		}
+		for (int y = 0; y < 10; y++) {
+			printf("at %i has %i\n", y, steps[y]);
+		}
+		vector<ImageDiffVal> topvals = GetTopXGoodones(result, 5);
+		for (int y = 0; y < topvals.size(); y++) {
+			printf("top at %i %i has %f\n", topvals[y].x, topvals[i].y, topvals[y].val);
+		}
+		namedWindow(result_windowName, WINDOW_NORMAL);
+		namedWindow(image_windowName, WINDOW_NORMAL);
+		imshow(image_windowName, img);
+		imshow(result_windowName, result);
+		break;
 	}
 }
 int main(int argc, char** argv)
 {	
-	doTopNumbers(); waitKey(0);return 0;
+	RecoList checkList = LoadDataInfo("data\\check\\bottom");
+	int thd = 200;
+	doTopNumbers(BlockInfo(Rect(780, 70,-1, 40), thd, "gld"));
+	doTopNumbers(BlockInfo(Rect(780, 135,-1, 30), thd,"elis"));
+	doTopNumbers(BlockInfo(Rect(280, 635, -1,45), thd, "bottom"));
+
+	Mat img = getGrayScale(imread("data\\cctxt\\archerl1.JPG", IMREAD_COLOR));
+	Mat numBlock = loadImageRect(img, BlockInfo(Rect(280, 635, -1, 45)));
+	DoReco(checkList, numBlock);
+	waitKey(0);
+	return 0;
 	//! [load_image]
 	/// Load image and template
 	Mat dOrigImg = getGrayScale(imread("data\\cctxt\\lib1.jpg", IMREAD_COLOR));
--- stdafx.h
+++ stdafx.h
@@ -15,6 +15,6 @@
 #include "opencv2/highgui.hpp"
 #include "opencv2/imgproc.hpp"
 #include <iostream>
-
+#include "dirent.h"
 
 // TODO: reference additional headers your program requires here
