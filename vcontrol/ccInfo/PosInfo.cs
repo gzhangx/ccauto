44586ebee26191a46daa4107c0a0f99b505af03e
Gang Zhang <gzhangx@hotmail.com>
2017-07-29 00:15:37 -0400

    adding move

---
+++ vcontrol/ccInfo/PosInfo.cs
@@ -0,0 +1,21 @@
+﻿
+
+namespace ccInfo
+{
+    public class ccPoint
+    {
+        public int x;
+        public int y;
+        public ccPoint(int xx, int yy)
+        {
+            x = xx;
+            y = yy;
+        }
+    }
+
+    public class PosInfo
+    {
+        public string name;
+        public ccPoint point;
+    }
+}
--- vcontrol/ccUi/MainWindow.xaml.cs
+++ vcontrol/ccUi/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ccInfo;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -24,6 +25,13 @@ namespace ccUi
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        List<PosInfo> points = new List<PosInfo>();
+        class GPInfo
+        {
+            public PosInfo pinfo;
+            public Ellipse fill;  
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
@@ -52,6 +60,44 @@ namespace ccUi
                 i.Stretch = Stretch.Uniform;                
                 canvas.Children.Clear();
                 canvas.Children.Add(i);
+            }
+        }
+
+        private void canvas_MouseMove(object sender, MouseEventArgs e)
+        {
+            info.Text = e.GetPosition(canvas).X+ "," +e.GetPosition(canvas).Y;
+        }
+
+        List<GPInfo> gpInfo = new List<GPInfo>();
+        private void canvas_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            var data = new PosInfo { name = txtName.Text, point = new ccPoint((int)e.GetPosition(canvas).X, (int)e.GetPosition(canvas).Y) };
+            if (points.Any(p =>
+            {
+                return Math.Abs(p.point.x - data.point.x) < 10
+                && Math.Abs(p.point.y - data.point.y) < 10;
+            })) return;
+            points.Add(data);            
+            Ellipse ele = new Ellipse();
+            ele.Width = 10;
+            ele.Height = 10;
+            ele.Fill = Brushes.Aqua;
+            canvas.Children.Add(ele);
+            Canvas.SetLeft(ele, data.point.x -  (ele.Width / 2));
+            Canvas.SetTop(ele, data.point.y - (ele.Height / 2));
+            ele.MouseLeftButtonUp += Ele_MouseLeftButtonUp;
+            var store = new GPInfo { pinfo = data, fill = ele };
+            gpInfo.Add(store);
+        }
+
+        private void Ele_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            var found = gpInfo.FirstOrDefault(itm => itm.fill == sender);
+            if (found != null)
+            {
+                canvas.Children.Remove(found.fill);
+                gpInfo.Remove(found);
+                points.Remove(found.pinfo);
             }
         }
     }
