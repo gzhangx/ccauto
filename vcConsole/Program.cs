da5c64d85c417c027d4df8ee60a0dee3f91ed3b5
Gang Zhang <gzhangx@hotmail.com>
2017-08-03 21:13:21 -0400

    adding external loop

--- vcConsole/Program.cs
+++ vcConsole/Program.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace vcConsole
@@ -11,7 +12,13 @@ namespace vcConsole
     {
         static void Main(string[] args)
         {
-            ccVcontrol.Program.Start(new SimpleController());
+            while (true)
+            {
+                ccVcontrol.Program.Start(new SimpleController());
+                Console.WriteLine("Sleeping");
+                Thread.Sleep(1000 * 60 * 10);
+                Console.WriteLine("Done Sleeping");
+            }
         }
     }
 
--- vcontrol/SwitchAccount.cs
+++ vcontrol/SwitchAccount.cs
@@ -85,7 +85,7 @@ namespace ccVcontrol
         public override StepContext Process()
         {
             account = CheckAccount() - 1;
-            Console.WriteLine($"Trying to find SINGLEMATCH for settings button account {account}");
+            context.InfoLog($"Trying to find SINGLEMATCH for settings button account ----- {account}");
             switchSteps.First(r => r.name == "SwitchAccount").Act = SwitchAccountAction;
             var res = DoSteps(switchSteps);
             InitGame("Account_" + account);
