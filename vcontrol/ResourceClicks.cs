7dab532fe6b763b7f89c37b01ef40f8c571b038e
Gang Zhang <gzhangx@hotmail.com>
2017-12-06 22:10:06 -0500

    fix rearm all

--- vcontrol/ResourceClicks.cs
+++ vcontrol/ResourceClicks.cs
@@ -50,9 +50,9 @@ namespace ccVcontrol
             context.MoveMouseAndClick(cmd);
 
             var cmds = ldr.ProcessingWithRetry();
-            cmd = cmds.Where(c=>c.cmpRes < 30000).OrderBy(c => c.cmpRes).FirstOrDefault();
+            cmd = cmds.Where(c=>c.cmpRes < 30000 && c.extraInfo.Contains("rearmall")).OrderBy(c => c.cmpRes).FirstOrDefault();
             //cmd = cmds.FirstOrDefault(c => c.extraInfo.Contains("rearmall") && c.cmpRes < 30000);
-            if (cmd == null || !cmd.extraInfo.Contains("rearmall"))
+            if (cmd == null)
             {
                 context.InfoLog("Warning, didn't find rearm all");
                 return;
